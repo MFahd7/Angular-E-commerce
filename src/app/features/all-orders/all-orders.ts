880eb27213298ee3b52312d231daac793a798c33
MFahd7 <fadalmaznee@gmail.com>
2025-09-19 14:27:20 +0300

    update from house

--- src/app/features/all-orders/all-orders.ts
+++ src/app/features/all-orders/all-orders.ts
@@ -2,10 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-all-orders',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './all-orders.html',
   styleUrl: './all-orders.css'
 })
-export class AllOrders {
+export class AllOrders implements OnInit {
+  private readonly ordersService = inject(AllordersService);
+  private readonly authService = inject(AuthService);
+
+  orders: Orders[] = [];
+
+  ngOnInit(): void {
+  this.getOrders();
+  }
+  getOrders():void{
+      const userId = this.authService.getUserIdFromToken();
+    if (userId) {
+      this.ordersService.getUserOrders(userId).subscribe({
+        next: (res) => {
+          console.log(res);
+          
+          this.orders = res;
+        },
+        
+      });
+    }
+  }
 
 }
